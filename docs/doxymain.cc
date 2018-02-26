7342c56458d8f1749537dcc9482ac4842d1761b6
Francesco W <fwanderlingh@gmail.com>
2018-02-26 14:37:11 +0100

    Added doxy

---
+++ docs/doxymain.cc
@@ -0,0 +1,10 @@
+/*!\mainpage Documentation Index
+ *
+ * \section intro_sec Introduction
+ *
+ * This is the doxygen documentation for the RML Library.
+ *
+ * \section descr_sec Description
+ *
+ * 
+ */
